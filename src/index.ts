ca1ce61e0d5d4c55cec6757c4acf84d2e98ef61e
haykeshjohn <john@haykeshgames.com>
2021-11-03 17:52:52 -0400

    Part 2 of shakuro tutorial
    * Phaser game setup
    * Loading scene with player sprite rendering

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,61 @@
-console.log('Hello, World!');
+import { Game, Types } from 'phaser';
+import { LoadingScene } from './scenes';
+
+
+const gameConfig: Types.Core.GameConfig = {
+    title: 'Derelict',
+    type: Phaser.AUTO,
+    parent: 'game',         // element id to render the game into
+    backgroundColor: '#333333',
+    scale: {
+        mode: Phaser.Scale.ScaleModes.NONE,
+        width: window.innerWidth,
+        height: window.innerHeight
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false    // TODO: Is this really necessary? Why would this be the default?
+        }
+    },
+    render: {
+        antialiasGL: false,
+        pixelArt: true
+    },
+    callbacks: {
+        postBoot: () => {
+            window.sizeChanged();
+        }
+    },
+    canvasStyle: 'display: block: width: 100%; width: 100%;',
+    autoFocus: true,
+    audio: {
+        disableWebAudio: false
+    },
+    scene: [LoadingScene]
+};
+
+window.sizeChanged = () => {
+    if (window.game.isBooted) {
+        setTimeout(() => {
+        window.game.scale.resize(window.innerWidth, window.innerHeight);
+        window.game.canvas.setAttribute(
+            'style',
+            `display: block: width: ${window.innerWidth}; height: ${window.innerHeight}`        );
+
+        }, 100);
+    }
+};
+
+window.onresize = () => window.sizeChanged();
+
+// Setup our game instance
+window.game = new Game(gameConfig);
+
+// Patch the Window interface to avoid the errors
+declare global {
+    interface Window {
+        sizeChanged: () => void;
+        game: Phaser.Game;
+    }
+}
