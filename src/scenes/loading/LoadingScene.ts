bc32f6404268e92ad3454b2de26cedc31140f87b
Tom K <tom@haykeshgames.com>
2021-11-10 10:00:46 -0500

    Remove unused player assets.

--- src/scenes/loading/LoadingScene.ts
+++ src/scenes/loading/LoadingScene.ts
@@ -10,16 +10,10 @@ export class LoadingScene extends Scene {
     preload(): void {
         this.load.baseURL = 'assets/';
         
-        // First param is the key for looking up the asset, second param is the relative path
-        // from the baseURL we set above.
-        this.load.image('king', 'sprites/king.png');
         this.load.spritesheet('player_spr', 'spritesheets/player-32-32.png', {
             frameWidth: 32,
             frameHeight: 32,
         });
-
-        // Atlas for the king animations
-        this.load.atlas('a-king', 'spritesheets/a-king.png', 'spritesheets/a-king_atlas.json');
 
         this.load.image({key: 'tiles', url: 'tilemaps/tiles/dungeon-16-16.png'});
         this.load.tilemapTiledJSON('dungeon', 'tilemaps/json/level1.json');
