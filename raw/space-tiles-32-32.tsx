7cde5c647561708d5eb1a8a1b0d246825a51399b
Tom K <tom@haykeshgames.com>
2021-11-10 08:51:08 -0500

    Add new assets and simple level.

---
+++ raw/space-tiles-32-32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="space-tiles-32-32.png" tilewidth="32" tileheight="32" tilecount="851" columns="37">
+ <image source="../../Downloads/Tech Dungeon Roguelite - Asset Pack (v2)/tileset x1.png" width="1184" height="736"/>
+</tileset>
